6ef78b6f22b740074985ecd5e88514f9c58452dc
João Lucca Trench <jltrench@gmail.com>
2021-10-11 02:59:26 -0300

    Fixture BeforeEach

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscatalog.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,9 +13,18 @@ import com.devsuperior.dscatalog.entities.Product;
 
 @DataJpaTest
 public class ProductRepositoryTests {
+	
+	private long existingId;
+	private long nonExistingId;
 
 	@Autowired
 	private ProductRepository repository;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		existingId = 1L;
+		nonExistingId = 1000L;
+	}
 	
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
